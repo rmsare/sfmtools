3664940af7cfe816d394a235a2cace3e7fe32dbb
Robert Sare <rmsare@stanford.edu>
2016-09-03 10:58:17 -0700

    Align photos and remove unaligned photos

---
+++ sfmtools.py
@@ -0,0 +1,20 @@
+""" Utility functions for PhotoScan processing """
+
+import os
+import PhotoScan
+
+def align_and_clean_photos(chunk):
+    ncameras = len(chunk.cameras)
+    for frames in chunk.frames:
+        frame.matchPhotos()
+
+    chunk.alignCameras()
+    for camera in chunk.cameras:
+        if camera.transform is None:
+            chunk.remove(camera)
+    
+    naligned = len(chunk.cameras)
+    print('%d/%d cameras aligned' % naligned, ncameras)
+
+
+    
